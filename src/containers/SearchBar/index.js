bb0dfc306afb8fbfeaa9cff921bc40a711c87f69
warlyware <danward@gmail.com>
2018-03-25 16:26:09 -0400

    Make API request to GiantBomb in SearchBar component

--- src/containers/SearchBar/index.js
+++ src/containers/SearchBar/index.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import API_KEY from '../../API_KEY';
+
+const giantBombApiUrl = `
+  https://www.giantbomb.com/api/search/?api_key=${API_KEY}&format=JSON
+`;
 
 export default class SearchBar extends Component {
   state = {
@@ -6,15 +13,24 @@ export default class SearchBar extends Component {
   }
 
   onInputChange = (event) => {
-    event.preventDefault();
     this.setState({
       searchTerm: event.target.value
     });
   }
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    axios.get(`${giantBombApiUrl}&query=${this.state.searchTerm}`)
+    .then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
   render() {
     return(
-      <form className="container">
+      <form onSubmit={this.onFormSubmit} className="container">
           <input
             placeholder="Search for Games"
             type="text"
@@ -25,6 +41,5 @@ export default class SearchBar extends Component {
       </form>
     );
   }
-
 }
 
