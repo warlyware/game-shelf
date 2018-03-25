194f6def08aa13f1ce2e8fb29aecd57f7faab482
warlyware <danward@gmail.com>
2018-03-25 16:35:31 -0400

    Add redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { ReduxPromise } from 'redux-promise';
+
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStore = applyMiddleware(ReduxPromise)(createStore);
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
